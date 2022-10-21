5fe0e55e2b316d4b4977a723c25e52df2491b2cb
Clement MARCEL <clmtmarcel@gmail.com>
2022-10-21 15:58:50 +0200

    add urls

--- app/settings.py
+++ app/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
-    "app",
-    "api",
+    "projects",
+    "authentication",
 ]
 
 REST_FRAMEWORK = {
