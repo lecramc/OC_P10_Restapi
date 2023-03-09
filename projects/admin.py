cb58b8b65116a8494633f8b72a83b17b9c4392ce
Clement MARCEL <clmtmarcel@gmail.com>
2023-03-09 10:08:27 +0100

    view in django admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Project, Issue, Comment, Contributor
+
 # Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    pass
